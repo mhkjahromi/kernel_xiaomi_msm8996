3cbc347eb982ec1c5564458f49bea25554977238
xNombre <kartapolska@gmail.com>
2019-01-17 23:34:05 +0100

    sound: soc: codecs: import tfa98xx driver v6.5.5
    * sound: soc: codecs: nuke tfa98xx driver
    
    We're preparing for new one.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    
    * sound: soc: codecs: import tfa98xx driver v6.5.5
    
    This is new tfa98xx driver imported from codeaurora git.
    In next commits we will restore previous functionality of this driver.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    
    * sound: soc: codecs: support tfa9891 firmware
    
    This re-adds xiaomi custom change to firmware file selection.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    
    * sound: soc: codecs: set codec pointer in tfa98xx
    
    This re-adds sound control interface to modify speaker gain.
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    
    * sound: soc: codecs: disable tfa98xx debugging
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    
    * fix tfa clang error, wercker build with clang
    
    * sound: fx compile wcd9335 with new tfa
    
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>
    Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>

--- sound/soc/codecs/tfa98xx/tfa98xx_tfafieldnames.h
+++ sound/soc/codecs/tfa98xx/tfa98xx_tfafieldnames.h
@@ -1,7 +1,15 @@
+/*
+ * Copyright (C) 2014 NXP Semiconductors, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
 
 typedef struct TfaBfName {
-	unsigned short bfEnum;
-	char  *bfName;
+   unsigned short bfEnum;
+   char  *bfName;
 } tfaBfName_t;
 
 typedef struct TfaIrqName {
@@ -15,6 +23,12 @@ typedef struct TfaIrqName {
 #include "tfa9887_tfafieldnames.h"
 #include "tfa9890_tfafieldnames.h"
 #include "tfa9891_tfafieldnames.h"
+#include "tfa9872_tfafieldnames.h"
+#include "tfa9912_tfafieldnames.h"
+#include "tfa9896_tfafieldnames.h"
+#include "tfa9874_tfafieldnames.h"
+#include "tfa9894_tfafieldnames.h"
+#include "tfa9894_tfafieldnames_N2.h"
 
 /* missing 'common' defs break the build but unused in TFA1 context */
 #define TFA1_BF_AMPINSEL -1
@@ -34,11 +48,13 @@ typedef struct TfaIrqName {
 #define TFA1_BF_ACKDMG	 -1
 #define TFA1_BF_SSRIGHTE -1
 #define TFA1_BF_SSLEFTE	 -1
+#define TFA1_BF_R25CL	 -1
+#define TFA1_BF_R25CR	 -1
 #define TFA1_BF_SWPROFIL 0x8045    /*!< profile save   */
 #define TFA1_BF_SWVSTEP  0x80a5    /*!< vstep save  */
 
-/* missing 'common' defs break the build but unused in TFA2 context */
-#define TFA2_BF_CFSM	-1
+/* missing 'common' defs break the build */
+#define TFA2_BF_CFSM -1
 
 
 /* MTP access uses registers
@@ -51,6 +67,22 @@ typedef struct TfaIrqName {
 /* interrupt enable register uses HW name in TFA2 */
 #define TFA2_BF_INTENVDDS TFA2_BF_IEVDDS
 
+
+/* TFA9891 specific bit field names */
+#define TFA1_BF_SAAMGAIN 0x2202
+#define TFA2_BF_SAAMGAIN -1
+
+/* TFA9872 specific bit field names */
+#define TFA2_BF_IELP0 TFA9872_BF_IELP0
+#define TFA2_BF_ISTLP0 TFA9872_BF_ISTLP0
+#define TFA2_BF_IPOLP0 TFA9872_BF_IPOLP0
+#define TFA2_BF_IELP1 TFA9872_BF_IELP1
+#define TFA2_BF_ISTLP1 TFA9872_BF_ISTLP1
+#define TFA2_BF_IPOLP1 TFA9872_BF_IPOLP1
+#define TFA2_BF_LP0 TFA9872_BF_LP0
+#define TFA2_BF_LP1 TFA9872_BF_LP1
+#define TFA2_BF_R25C TFA9872_BF_R25C
+#define TFA2_BF_SAMMODE TFA9872_BF_SAMMODE
 
 /* interrupt bit field names of TFA2 and TFA1 do not match */
 #define TFA1_BF_IEACS TFA1_BF_INTENACS
@@ -87,3 +119,23 @@ typedef struct TfaIrqName {
 #define TFA1_BF_IESPKS -1
 #define TFA1_BF_ISTSPKS -1
 #define TFA1_BF_IPOSPKS -1
+#define TFA1_BF_IECLKS -1
+#define TFA1_BF_ISTCLKS -1
+#define TFA1_BF_IPOCLKS -1
+#define TFA1_BF_IEAMPS -1
+#define TFA1_BF_ISTAMPS -1
+#define TFA1_BF_IPOAMPS -1
+#define TFA1_BF_IELP0 -1
+#define TFA1_BF_ISTLP0 -1
+#define TFA1_BF_IPOLP0 -1
+#define TFA1_BF_IELP1 -1
+#define TFA1_BF_ISTLP1 -1
+#define TFA1_BF_IPOLP1 -1
+#define TFA1_BF_LP0 -1
+#define TFA1_BF_LP1 -1
+#define TFA1_BF_R25C -1
+#define TFA1_BF_SAMMODE	 -1
+
+/* TDM STATUS fields not available on TFA1 */
+#define TFA1_BF_TDMLUTER -1
+#define TFA1_BF_TDMERR -1
